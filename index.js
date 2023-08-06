bab7f1f24f292cc97e781ebdc390f4c1fda69e93
Bonface <bonniewanga34@gmail.com>
2023-08-07 00:05:42 +0300

    setting up the second event listener

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
    // The first event listener listenes for the DOM to load.
    document.addEventListener('DOMContentLoaded', async() => {
 
+    // The second event listener listens for a click on the buttons in the page and displays an alert.
+    document.getElementById('join').addEventListener('click', ()=> {
+        console.log(alert('Hi there, thank you for joining'))
+    })
+    document.getElementById('time-schedule').addEventListener('click', () => {
+        console.log(alert('Hi there, you will be seeing the schedule shortly'))
+    })
+   
+
+
+
+
+
    })
